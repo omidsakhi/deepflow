21d900b4ff4c34a717931a4afdd9786efe7060dc
Omid Sakhi <omid.sakhi@gmail.com>
2017-06-11 03:53:06 -0400

    Added prep script

---
+++ scripts/prep-socket.io-client.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+cd..
+cd third-party
+cd socket.io-client-cpp
+mkdir cmake-build
+cd cmake-build
+cmake -G "Visual Studio 14 2015 Win64" -DBOOST_ROOT:STRING="D:\Projects\boost_1_64_0" -DBOOST_VER:STRING="1.64.0" ../
